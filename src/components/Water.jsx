307aec9eadc39377babf762aa95bf10c562544f6
poloman719 <poloman7190@gmail.com>
2024-02-23 11:47:56 -0800

    added normal map

--- src/components/Water.jsx
+++ src/components/Water.jsx
@@ -2,12 +2,13 @@ import { useLoader } from "@react-three/fiber";
 import { TextureLoader } from "three";
 
 const Water = () => {
-  const height = useLoader(TextureLoader, "water.jpg");
+  const height = useLoader(TextureLoader, "water_height.jpg");
+  const normal = useLoader(TextureLoader, "water_normal.png");
 
   return (
-    <mesh rotation={[-Math.PI / 2, 0, 0]}>
+    <mesh rotation={[-Math.PI / 2, 0, 0]} scale={[2, 1, 0.2]}>
       <planeGeometry args={[3, 5, 1024, 1024]} />
-      <meshStandardMaterial displacementMap={height} />
+      <meshStandardMaterial displacementMap={height} normalMap={normal}/>
     </mesh>
   );
 };
